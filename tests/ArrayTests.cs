5b16cdee8edec29f9d0ea6f04785a999e02f50a4
Robert Pecz <rob.pecz@gmail.com>
2025-06-05 12:43:06 +0200

    [WIP] Add various tests to the ArrayTests.cs.

--- tests/ArrayTests.cs
+++ tests/ArrayTests.cs
@@ -12,7 +12,7 @@ namespace LeetCodeTests
         [TestCase(new int[] { 2, 7, 11, 15 }, 9, ExpectedResult = new int[] { 0, 1 })]
         [TestCase(new int[] { 3, 2, 4 }, 6, ExpectedResult = new int[] { 1, 2 })]
         [TestCase(new int[] { 3, 3 }, 6, ExpectedResult = new int[] { 0, 1 })]
-        public int[] TwoSumsSolution(int[] nums, int target)
+        public int[] TwoSumsSolutionTests(int[] nums, int target)
         {
             TwoSumsSolution solution = new TwoSumsSolution();
             return solution.TwoSum(nums, target);
@@ -20,7 +20,7 @@ namespace LeetCodeTests
 
         [TestCase(new int[] { 3, 1, 2, 4 }, ExpectedResult = new int[] { 2, 4, 3, 1 })]
         [TestCase(new int[] { 0 }, ExpectedResult = new int[] { 0 })]
-        public int[] ArrayParitySolution(int[] nums)
+        public int[] ArrayParitySolutionTests(int[] nums)
         {
             ArrayParitySolution arrayParitySolution = new ArrayParitySolution();
             return arrayParitySolution.SortArrayByParity(nums);
@@ -28,7 +28,7 @@ namespace LeetCodeTests
 
         [TestCase(new int[] { 3, 1, 2, 4 }, ExpectedResult = new int[] { 1, 2, 3 })]
         [TestCase(new int[] { 1, 1 }, ExpectedResult = new int[] { 2 })]
-        public int[] DisappearedNumberSolution(int[] nums)
+        public int[] DisappearedNumberSolutionTests(int[] nums)
         {
             FindDisappearedNumberSolution findDisappearedNumberSolution = new FindDisappearedNumberSolution();
             return findDisappearedNumberSolution.FindDisappearedNumbers(nums);
@@ -36,10 +36,55 @@ namespace LeetCodeTests
 
         [TestCase(new int[] { 1, 0, 2, 3, 0, 4, 5, 0 }, ExpectedResult = new int[] { 1, 0, 0, 2, 3, 0, 0, 4 })]
         [TestCase(new int[] { 1, 2, 3 }, ExpectedResult = new int[] { 1, 2, 3, })]
-        public int[] DuplicateZeroesSolution(int[] nums)
+        public int[] DuplicateZeroesSolutionTests(int[] nums)
         {
             DoubleZerosSolution doubleZerosSolution = new DoubleZerosSolution();
             return doubleZerosSolution.DoubleZero(nums);
+        }
+
+        [TestCase(new int[] { 12, 345, 2, 6, 7896 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 555, 901, 482, 1771 }, ExpectedResult = 1)]
+        public int EvenNumberDigitsSolutionTests(int[] nums)
+        {
+            EvenNumberDigitsSolution evenNumberDigitsSolution = new EvenNumberDigitsSolution();
+            return evenNumberDigitsSolution.FindNumbers(nums);
+        }
+
+        [TestCase(new int[] { 1, 1, 4, 2, 1, 3 }, ExpectedResult = 3)]
+        [TestCase(new int[] { 5, 1, 2, 3, 4 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 0)]
+        public int HeightCheckerSolutionTests(int[] nums)
+        {
+            HeightCheckerSolution heightCheckerSolution = new HeightCheckerSolution();
+            return heightCheckerSolution.HeightChecker(nums);
+        }
+
+        [TestCase(new int[] { 1, 1, 0, 1, 1, 1 }, ExpectedResult = 3)]
+        [TestCase(new int[] { 1, 0, 1, 1, 0, 1 }, ExpectedResult = 2)]
+        public int MaxConsSolutionTests(int[] nums)
+        {
+            MaxConsSolution maxConsSolution = new MaxConsSolution();
+            return maxConsSolution.FindMaxConsecutiveOnes(nums);
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3)]
+        [TestCase(new int[] { 1 }, 1, new int[] { }, 0)]
+        [TestCase(new int[] { 0 }, 0, new int[] { 1 }, 1)]
+        public void MergeTwoArraySolutionTests(int[] nums1, int m, int[] nums2, int n)
+        {
+            MergeTwoArraySolution mergeTwoArraySolution = new MergeTwoArraySolution();
+            mergeTwoArraySolution.Merge(nums1, m, nums2, n);
+            var testArray = new int[] { 1, 2, 2, 3, 5, 6 };
+            var isTheTwoArrayHasTheSameElements = nums1.SequenceEqual(testArray);
+        }
+
+        [TestCase(new int[] { 2, 1 }, ExpectedResult = false)]
+        [TestCase(new int[] { 3, 5, 5 }, ExpectedResult = false)]
+        [TestCase(new int[] { 0, 3, 2, 1 }, ExpectedResult = true)]
+        public bool MountainArraySolutionTests(int[] nums)
+        {
+            MountainArraySolution mountainArraySolution = new MountainArraySolution();
+            return mountainArraySolution.ValidMountainArray(nums);
         }
     }
 }
